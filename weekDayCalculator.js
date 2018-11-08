0d2215653c332b23139b7d2e357f9656f72cdfc5
Luke Towell <luke.towell@hotmail.com>
2018-11-08 19:30:33 +0000

    Initial Commit and code snippet for the day of the week calculation

---
+++ weekDayCalculator.js
@@ -0,0 +1,27 @@
+var date = new Date("2018-11-16").getDay()
+var day = ""
+
+switch(date){
+case 1:
+	day = "Monday";
+    break;
+case 2:
+	day = "Tuesday";
+    break;
+case 3: 
+    day = "Wednesday";
+    break;
+case 4: 
+    day = "Thursday";
+    break;   
+case 5: 
+    day = "Friday";
+    break;    
+case 6: 
+    day = "Saturday";
+    break; 
+    
+default: 
+    day = "Sunday";
+    break;
+}
